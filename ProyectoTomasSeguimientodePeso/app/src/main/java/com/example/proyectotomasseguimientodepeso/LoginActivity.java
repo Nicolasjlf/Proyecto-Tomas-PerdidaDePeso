78189a98cf306b5a97ed9572bdd54238704f7cf8
Lopi <nicolasjlf@hotmail.com>
2019-10-04 17:45:09 -0300

    MainActivity Terminada

--- ProyectoTomasSeguimientodePeso/app/src/main/java/com/example/proyectotomasseguimientodepeso/LoginActivity.java
+++ ProyectoTomasSeguimientodePeso/app/src/main/java/com/example/proyectotomasseguimientodepeso/LoginActivity.java
@@ -3,12 +3,19 @@ package com.example.proyectotomasseguimientodepeso;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button botonLogin;
+    private Button botonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        botonLogin = findViewById(R.id.activityLogin_buttonLogin);
+        botonRegister = findViewById(R.id.activityLogin_buttonRegister);
     }
 }
--- ProyectoTomasSeguimientodePeso/app/src/main/java/com/example/proyectotomasseguimientodepeso/MainActivity.java
+++ ProyectoTomasSeguimientodePeso/app/src/main/java/com/example/proyectotomasseguimientodepeso/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.proyectotomasseguimientodepeso;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botonMyAccount;
+    private Button botonHistory;
+    private Button botonSettings;
+    private Button botonMyPics;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        botonMyAccount = findViewById(R.id.mainActivity_myAcount);
+        botonHistory = findViewById(R.id.mainActivity_histoy);
+        botonSettings= findViewById(R.id.mainActivity_settings);
+        botonMyPics= findViewById(R.id.mainActivity_myPics);
+
+        botonMyAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,AccountActivity.class);
+                startActivity(intent);
+            }
+        });
+        botonMyPics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,PhotosActivity.class);
+                startActivity(intent);
+            }
+        });
+        botonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+        botonHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,HistoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
