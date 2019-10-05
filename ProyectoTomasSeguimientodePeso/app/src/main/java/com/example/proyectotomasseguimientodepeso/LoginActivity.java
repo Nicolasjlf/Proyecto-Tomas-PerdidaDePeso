36a304ffd6f17095c7c38748dfd1602d09a4c1c6
Lopi <nicolasjlf@hotmail.com>
2019-10-05 11:35:59 -0300

    Firebase Login Terminado

--- ProyectoTomasSeguimientodePeso/app/src/main/java/com/example/proyectotomasseguimientodepeso/LoginActivity.java
+++ ProyectoTomasSeguimientodePeso/app/src/main/java/com/example/proyectotomasseguimientodepeso/LoginActivity.java
@@ -1,14 +1,31 @@
 package com.example.proyectotomasseguimientodepeso;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button botonLogin;
     private Button botonRegister;
+
+    private EditText editTextEmail;
+    private EditText editTextpass;
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +34,82 @@ public class LoginActivity extends AppCompatActivity {
 
         botonLogin = findViewById(R.id.activityLogin_buttonLogin);
         botonRegister = findViewById(R.id.activityLogin_buttonRegister);
+        editTextEmail = findViewById(R.id.activityLogin_username);
+        editTextpass = findViewById(R.id.activityLogin_pasword);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        botonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editTextEmail.getText().toString();
+                String password = editTextpass.getText().toString();
+                crearUsuario(email,password);
+            }
+        });
+        botonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editTextEmail.getText().toString();
+                String password = editTextpass.getText().toString();
+                loguearUsuario(email,password);
+            }
+        });
+
+
+
+    }
+
+
+    private void crearUsuario(String email, String password){
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void loguearUsuario(String email,String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            irAMain();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void irAMain(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
