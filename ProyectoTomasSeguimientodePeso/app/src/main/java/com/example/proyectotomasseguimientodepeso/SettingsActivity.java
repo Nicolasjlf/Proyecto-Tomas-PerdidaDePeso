fb94967d5f6127bdd88741564ca45e5296b9f7eb
Pat <patriciomugarte@gmail.com>
2019-10-05 18:47:44 -0300

    Listo el Layout de la SettingActivity

--- ProyectoTomasSeguimientodePeso/app/src/main/java/com/example/proyectotomasseguimientodepeso/SettingsActivity.java
+++ ProyectoTomasSeguimientodePeso/app/src/main/java/com/example/proyectotomasseguimientodepeso/SettingsActivity.java
@@ -2,13 +2,52 @@ package com.example.proyectotomasseguimientodepeso;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private Switch seleccionSistemaMetricoSwitch;
+    private TextView sistemaIntenacionalTextView;
+    private TextView sistemaImperialTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        seleccionSistemaMetricoSwitch = findViewById(R.id.activitySettings_SwitchSistemaMetrico);
+        sistemaIntenacionalTextView = findViewById(R.id.activitySettings_SistemaInternacional_TextView);
+        sistemaImperialTextView = findViewById(R.id.activitySettings_SistemaImperial_TextView);
+
+        seleccionSistemaMetricoSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    marcarSeleccion(sistemaImperialTextView);
+                    quitarSeleccion(sistemaIntenacionalTextView);
+                } else {
+                    marcarSeleccion(sistemaIntenacionalTextView);
+                    quitarSeleccion(sistemaImperialTextView);
+                }
+            }
+        });
+
+    }
+
+    private void marcarSeleccion(TextView textViewSeleccionado) {
+        textViewSeleccionado.setBackgroundColor(Color.GRAY);
+        textViewSeleccionado.setTextColor(Color.WHITE);
+        textViewSeleccionado.setTypeface(Typeface.DEFAULT_BOLD);
+    }
+
+    private void quitarSeleccion(TextView textViewSeleccionado) {
+        textViewSeleccionado.setBackgroundColor(Color.TRANSPARENT);
+        textViewSeleccionado.setTextColor(Color.GRAY);
+        textViewSeleccionado.setTypeface(Typeface.DEFAULT);
     }
 }
