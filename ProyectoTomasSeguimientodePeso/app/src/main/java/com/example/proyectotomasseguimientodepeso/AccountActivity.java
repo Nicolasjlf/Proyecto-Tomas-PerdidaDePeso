458623abc34013c8e2f1f3fa13dbb25b43751b1a
Pat <patriciomugarte@gmail.com>
2019-10-05 14:12:49 -0300

    Correcciones menores, Linkeo de AccountActivity a SettingsActivity. Principio de diseo de SettingsActivity.

--- ProyectoTomasSeguimientodePeso/app/src/main/java/com/example/proyectotomasseguimientodepeso/AccountActivity.java
+++ ProyectoTomasSeguimientodePeso/app/src/main/java/com/example/proyectotomasseguimientodepeso/AccountActivity.java
@@ -2,13 +2,28 @@ package com.example.proyectotomasseguimientodepeso;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AccountActivity extends AppCompatActivity {
+
+    private Button botonEditarInformacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        botonEditarInformacion = findViewById(R.id.activityAcount_EditInfo);
+
+        botonEditarInformacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AccountActivity.this,SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
