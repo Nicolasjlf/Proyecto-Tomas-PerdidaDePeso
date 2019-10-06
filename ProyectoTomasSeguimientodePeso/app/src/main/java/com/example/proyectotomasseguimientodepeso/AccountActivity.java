49f9d63c19ced86b34699db827468b99c1beba4f
Pat <patriciomugarte@gmail.com>
2019-10-05 21:47:59 -0300

    Implementada la RealtimeDatabase de Firebase, hice la prueba con el seteo de nombre de usuario en la SettingsActivity y la lectura del mismo en la AccountActivity desde el usuario.

--- ProyectoTomasSeguimientodePeso/app/src/main/java/com/example/proyectotomasseguimientodepeso/AccountActivity.java
+++ ProyectoTomasSeguimientodePeso/app/src/main/java/com/example/proyectotomasseguimientodepeso/AccountActivity.java
@@ -1,5 +1,6 @@
 package com.example.proyectotomasseguimientodepeso;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,19 +11,28 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AccountActivity extends AppCompatActivity {
 
     private Button botonEditarInformacion;
     private TextView textViewMailUser;
+    private TextView textViewNameUser;
+
+    private FirebaseDatabase firebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
-        botonEditarInformacion = findViewById(R.id.activityAcount_EditInfo);
-        textViewMailUser = findViewById(R.id.activityAcount_mailUser);
+        botonEditarInformacion = findViewById(R.id.activityAccount_EditInfo);
+        textViewMailUser = findViewById(R.id.activityAccount_mailUser);
+        textViewNameUser = findViewById(R.id.activityAccount_nombreUser);
 
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         String email = currentUser.getEmail();
@@ -36,5 +46,25 @@ public class AccountActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        String uid = currentUser.getUid();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference nameReference = firebaseDatabase.getReference(uid).child("nombre");
+        //TODO: Leer la referencia del POJO, no dato por dato
+
+        nameReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                CharSequence text = textViewNameUser.getText();
+                textViewNameUser.setText(text+value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
--- ProyectoTomasSeguimientodePeso/app/src/main/java/com/example/proyectotomasseguimientodepeso/SettingsActivity.java
+++ ProyectoTomasSeguimientodePeso/app/src/main/java/com/example/proyectotomasseguimientodepeso/SettingsActivity.java
@@ -5,15 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SettingsActivity extends AppCompatActivity {
 
     private Switch seleccionSistemaMetricoSwitch;
     private TextView sistemaIntenacionalTextView;
     private TextView sistemaImperialTextView;
+    private FirebaseDatabase firebaseDatabase;
+    private Button confirmChangesButton;
+    private EditText nombreEditText;
+    private EditText alturaEditText;
+    private EditText pesoEditText;
+    private EditText pesoObjetivoEditText;
+    private EditText pasosObjetivoEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +37,12 @@ public class SettingsActivity extends AppCompatActivity {
         seleccionSistemaMetricoSwitch = findViewById(R.id.activitySettings_SwitchSistemaMetrico);
         sistemaIntenacionalTextView = findViewById(R.id.activitySettings_SistemaInternacional_TextView);
         sistemaImperialTextView = findViewById(R.id.activitySettings_SistemaImperial_TextView);
+        nombreEditText = findViewById(R.id.activitySettings_Name_EditText);
+        alturaEditText = findViewById(R.id.activitySettings_Height_EditText);
+        pesoEditText = findViewById(R.id.activitySettings_Weight_EditText);
+        pesoObjetivoEditText = findViewById(R.id.activitySettings_GoalWeight_EditText);
+        pasosObjetivoEditText = findViewById(R.id.activitySettings_StepGoal_EditText);
+        confirmChangesButton = findViewById(R.id.activitySettings_Confirmar_Button);
 
         //TODO: Agarrar el booleano de si está en Sistema Internacional y hacer el setChecked() a la opción correspondiente del Switch
 
@@ -34,6 +54,18 @@ public class SettingsActivity extends AppCompatActivity {
                 } else {
                     seleccionarSistemaInternacional();
                 }
+            }
+        });
+
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        final DatabaseReference databaseReference = firebaseDatabase.getReference(uid);
+
+        confirmChangesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                databaseReference.child("nombre").setValue(nombreEditText.getText().toString());
+                //TODO: Pasar esto para que escriba un objeto Usuario (POJO), no dato por dato.
             }
         });
     }
